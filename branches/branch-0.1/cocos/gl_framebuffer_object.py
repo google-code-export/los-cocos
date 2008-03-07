1375b263321246baaa6676895fbe89979fba7e92
ricardoquesada <ricardoquesada@f663ce52-ac46-0410-b8de-c1c220b0eb76>
2008-03-07 17:22:28 +0000

    branch 0.1 from trunk. rev159

---
+++ branches/branch-0.1/cocos/gl_framebuffer_object.py
@@ -0,0 +1,43 @@
+"""A thin wrapper for OpenGL framebuffer objets. For implementation use only"""
+
+from ctypes import c_int, byref
+from pyglet.gl import *
+from pyglet.gl.glext_arb import *
+
+class FramebufferObject (object):
+    """
+    Wrapper for framebuffer objects. See
+    
+    http://oss.sgi.com/projects/ogl-sample/registry/EXT/framebuffer_object.txt
+    
+    API is not very OO, should be improved.
+    """
+    def __init__ (self):
+        """Create a new framebuffer object"""
+        id = c_ulong(0)
+        glGenFramebuffersEXT (1, byref(id))
+        self._id = id.value
+
+    def bind (self):
+        """Set FBO as current rendering target"""
+        glBindFramebufferEXT (GL_FRAMEBUFFER_EXT, self._id)
+
+    def unbind (self):
+        """Set default framebuffer as current rendering target"""
+        glBindFramebufferEXT (GL_FRAMEBUFFER_EXT, 0)
+
+    def texture2d (self, texture):
+        """Map currently bound framebuffer (not necessarily self) to texture"""
+        glFramebufferTexture2DEXT (
+            GL_FRAMEBUFFER_EXT,
+            GL_COLOR_ATTACHMENT0_EXT,
+            texture.target,
+            texture.id,
+            texture.level)
+
+    def check_status(self):
+        """Check that currently set framebuffer is ready for rendering"""
+        status = glCheckFramebufferStatusEXT (GL_FRAMEBUFFER_EXT)
+        if status != GL_FRAMEBUFFER_COMPLETE_EXT:
+            raise Exception ("Frambuffer not complete: %d" % status)
+
---
+++ branches/branch-0.1/test/test_effects.py
@@ -0,0 +1,106 @@
+#
+# Los Cocos: Effect Example
+# http://code.google.com/p/los-cocos/
+#
+
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from cocos.director import director
+from cocos.layer import Layer, ColorLayer
+from cocos.scene import Scene
+from cocos.effect import TextureFilterEffect, ColorizeEffect, RepositionEffect
+import pyglet
+from pyglet import font
+from pyglet.window import key
+from pyglet.gl import *
+
+class PictureLayer(Layer):
+
+    def __init__ (self, y):
+        self.x = 100
+        self.y = y
+        self.speed = 35
+        self.img = pyglet.image.load ('ball.png')
+    
+    def step (self, dt):
+        self.x += self.speed * dt
+        if self.x > 200 and self.speed > 0: self.speed = -self.speed
+        if self.x < 100 and self.speed < 0: self.speed = -self.speed
+        self.img.blit (self.x, self.y)
+
+class DynamicColorizeEffect (ColorizeEffect):
+    def __init__ (self):
+        super (ColorizeEffect, self).__init__ ()
+        self.color = (1,1,1,1)
+        self.timer = 0
+
+    def prepare (self, target, dt):
+        super (ColorizeEffect, self).prepare (target, dt)
+        self.timer += dt
+        # red glow: 
+        red = self.timer % 2
+        if red > 1: red = 2-red
+        # set color
+        self.color = (red,1,1,1)
+        
+class ControlLayer(Layer):
+
+    def on_enter( self ):
+        ft_title = font.load( None, 32 )
+        ft_subtitle = font.load( None, 18 )
+        ft_help = font.load( None, 16 )
+
+        self.text_title = font.Text(ft_title, "Effect Demos",
+            x=5,
+            y=480,
+            halign=font.Text.LEFT,
+            valign=font.Text.TOP)
+
+        self.text_subtitle = font.Text(ft_subtitle, effects[current_effect][0],
+            x=5,
+            y=400,
+            halign=font.Text.LEFT,
+            valign=font.Text.TOP)
+        
+        self.text_help = font.Text(ft_help,"Press LEFT / RIGHT for prev/next example",
+            x=320,
+            y=20,
+            halign=font.Text.CENTER,
+            valign=font.Text.CENTER)
+
+    def step(self, dt):
+        self.text_title.draw()
+        self.text_subtitle.text = effects[current_effect][0]
+        self.text_subtitle.draw()
+        self.text_help.draw()
+        
+    def on_key_press( self, k , m ):
+        global current_effect
+        if k == key.LEFT:
+            current_effect = (current_effect-1)%len(effects)
+            ball.set_effect(effects[current_effect][1])
+        if k == key.RIGHT:
+            current_effect = (current_effect+1)%len(effects)
+            ball.set_effect(effects[current_effect][1])
+
+
+current_effect = 0
+
+if __name__ == "__main__":
+    director.init()
+
+    effects = [
+        ("ball = Layer(...)\n"
+         " No effect", None),
+        ("ball.set_effect (ColorizeEffect(color=(0.5,1,0.5,0.65)))\n"
+         " Greenish hue, 65% opacity", ColorizeEffect(color=(0.5,1,0.5,0.65))),
+        ("ball.set_effect (RepositionEffect(width=director.get_window_size()[0]/2))\n"
+         " Horizontally scaled 50%", RepositionEffect(width=director.get_window_size()[0]/2)),
+        ("ball.set_effect (DynamicColorizeEffect())\n"
+         " Dynamic colorize effect made inheriting ColorizeEffect\n"
+         " and redefining prepare() to change self.color each frame", DynamicColorizeEffect()),
+              ]
+    ball = PictureLayer(240)
+    director.run( Scene (ColorLayer(0.1,0.1,0.2,1), ball, ControlLayer()) )
