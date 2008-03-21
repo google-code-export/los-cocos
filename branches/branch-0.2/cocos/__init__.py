c0a138da2e7474fe83bf61f618df063f4fbbeffb
ricardoquesada <ricardoquesada@f663ce52-ac46-0410-b8de-c1c220b0eb76>
2008-03-21 23:04:37 +0000

    added check for pyglet version

--- branches/branch-0.2/cocos/__init__.py
+++ branches/branch-0.2/cocos/__init__.py
@@ -8,6 +8,17 @@ __version__ = "0.2.0"
 __author__ = "PyAr Team"
 version = __version__
 
+def check_pyglet_version():
+    import pyglet
+    if pyglet.version.find( '1.0' ) != -1:
+        print "*" * 80
+        print "\ncocos v%s does not work with pyglet v%s" % (__version__, pyglet.version )
+        print "\nUse pyglet v1.1 or download to cocos-0.1.x\n"
+        print "*" * 80
+        raise Exception("Invalid pyglet version: %s" % pyglet.version)
+
+check_pyglet_version()
+
 import actions
 import director
 import effect
@@ -15,3 +26,4 @@ import layer
 import menu
 import path
 import scene
+
