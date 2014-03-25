df19f11e626575c4d1246b11f500c2840cf9c649
ccanepacc <ccanepacc@f663ce52-ac46-0410-b8de-c1c220b0eb76>
2014-03-25 01:49:36 +0000

    tagging r1371 as first revision supporting python 3.3+

---
+++ tags/py3.3+/utest/pyglet_mockup1/pyglet/app/__init__.py
@@ -0,0 +1,13 @@
+# app
+from __future__ import division, print_function, unicode_literals
+
+event_loop = None
+
+class EventLoop(object):
+    def __init__(self):
+        self.event = None
+    def run(self):
+        pass
+
+
+#event_loop = EventLoop()
